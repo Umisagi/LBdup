02a5be9e5a82b0b2d78ea0fbc0abb48ae04ea8ff
Umisagi <zenchiter@gmail.com>
2017-02-22 21:26:21 +0700

    Update chat.py

--- python/chat.py
+++ python/chat.py
@@ -9,6 +9,11 @@ if len(sys.argv) < 2:
   sys.exit(0)
 
 message = sys.argv[1]
+
+kernel = aiml.Kernel()
+kernel.learn("std-startup.xml")
+kernel.respond("load aiml b")
+
 if(message == "555"):
   result = "ขำ";
 else:
