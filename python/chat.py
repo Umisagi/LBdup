4a50ed656363ce428221045910206f3b1eaf6325
Umisagi <zenchiter@gmail.com>
2017-02-22 23:12:46 +0700

    Update chat.py

--- python/chat.py
+++ python/chat.py
@@ -1,18 +1,34 @@
 #!/usr/bin/python
 #-*-coding: utf-8 -*-
 
-import aiml
+
 import json
 import sys
+# define the function blocks
+def hello():
+    result = "สวัสดีเหี้ยไรสัส"
+def haha():
+    result = "ตลกเหรอ"
+def even():
+    print "n is an even number\n"
+def prime():
+    print "n is a prime number\n"
 
+# map the inputs to the function blocks
+options = {"สวัสดี" : hello,
+           "หวัดดี" : hello,
+           "ดี" : hello,
+           "ดีจ้า" : hello,
+           "555" : haha,
+           "ทดสอบ" : test,
+           "test" : test,
+           " " : test,
+           }
 if len(sys.argv) < 2:
   sys.exit(0)
 
 message = sys.argv[1]
 
-kernel = aiml.Kernel()
-kernel.learn("std-startup.xml")
-kernel.respond("load aiml b")
+option[message]()
 
-result = kernel.respond(message)
 print ("%s" % result)
