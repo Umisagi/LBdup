f04bf7a2dc67449d160fc732e571490288ae87e7
Umisagi <zenchiter@gmail.com>
2017-02-22 20:10:46 +0700

    Create chat.py

---
+++ python/chat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#-*-coding: utf-8 -*-
+
+import json
+import sys
+
+if len(sys.argv) < 2:
+  sys.exit(0)
+
+message = sys.argv[1]
+if(message == "555"):
+  result = "ขำ";
+else:
+  result = "ยังไม่ได้ตั้งค่าไว้";
+print ("%s" % result)
