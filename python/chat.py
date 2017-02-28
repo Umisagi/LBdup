bf03edaeec4ce71221a500791a94756a82f1ea2b
Umisagi <zenchiter@gmail.com>
2017-02-28 21:45:05 +0700

    Update chat.py

--- python/chat.py
+++ python/chat.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #-*-coding: utf-8 -*-
 
-
+import aiml
 import json
 import sys
 
@@ -9,17 +9,7 @@ if len(sys.argv) < 2:
   sys.exit(0)
 
 message = sys.argv[1]
-
-
-if (message == "สวัสดี"):
-    result = "สวัสดีครับ มีอะไรให้รับใช้ครับ"
-elif (message == "สวัสดี ครับ"):
-    result = "สวัสดีครับ มีอะไรให้รับใช้ครับ"
-elif (message == "ลาก่อน"):
-    result = "ลาก่อนครับ"
-elif (message == "ควย"):
-    result = "ไม่น่ารักเลยครับ"
-else:
-    result = "ระบบไม่สามารถประมวลผลคำที่ท่านส่งมาได้ ขออภัยด้วยครับ"
-
-print (str(message))
+kernel = aiml.Kernel()
+kernel.respond("load aiml b")
+result = kernel.respond(message)
+print result
